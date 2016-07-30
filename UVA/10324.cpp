3478321e30d1cdc0e99eabb6a71a46c21d265ec8
Elton Viana <eltonvs@outlook.com>
2016-07-30 02:52:49 -0300

    #UVA #10324 performance improvements

--- UVA/10324.cpp
+++ UVA/10324.cpp
@@ -7,12 +7,12 @@ int main() {
     while (scanf("%s", str) != EOF) {
         scanf("%i", &n);
         printf("Case %i:\n", c++);
-        for (int i = 0; i < n; i++) {
+        while (n--) {
             scanf("%i %i", &a, &b);
             if (a > b) a += b, b = a - b, a -= b;
             bool rst = true;
-            for (int j = a; j < b; j++)
-                rst &= (str[j] == str[j + 1]);
+            for (int i = a; i < b && rst; i++)
+                rst &= (str[i] == str[i + 1]);
             printf("%s\n", rst ? "Yes" : "No");
         }
     }
