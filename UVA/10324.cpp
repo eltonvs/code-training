cfd692dde5430dc7d6862b806d06769fb58b010d
Elton De Souza Vieira <eltonviana@A309-27L.imd.local>
2016-08-05 16:29:41 -0300

    Code improvements

--- UVA/10324.cpp
+++ UVA/10324.cpp
@@ -1,19 +1,21 @@
 #include <cstdio>
+#include <cstring>
 
 int main() {
     int n, a, b, c = 1;
     char str[1000005];
+    int map[1000005];
 
     while (scanf("%s", str) != EOF) {
+        int j = 0;
         scanf("%i", &n);
         printf("Case %i:\n", c++);
+        map[0] = 0;
+        for (int i = 1; str[i] != '\0'; i++)
+            map[i] = (str[i] == str[i - 1]) ? j : ++j;
         while (n--) {
             scanf("%i %i", &a, &b);
-            if (a > b) a += b, b = a - b, a -= b;
-            bool rst = true;
-            for (int i = a; i < b && rst; i++)
-                rst &= (str[i] == str[i + 1]);
-            printf("%s\n", rst ? "Yes" : "No");
+            printf("%s\n", map[a] == map[b] ? "Yes" : "No");
         }
     }
 
