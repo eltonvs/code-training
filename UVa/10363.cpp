a887883d54c2e509a45c880d1795fd101fb1f14a
Elton Viana <eltonvs@outlook.com>
2016-08-11 12:07:39 -0300

    #UVa - Starting 10363 (not working yet)

---
+++ UVa/10363.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstring>
+
+int main() {
+    int n;
+    char grid[3][4], buf;
+
+    scanf("%i", &n);
+
+    while (n--) {
+        int c_O = 0, c_X = 0, c_D = 0;
+        int w_X = 0, w_O = 0;
+        for (int i = 0; i < 3; i++) {
+            scanf("%s", grid[i]);
+            for (int j = 0; j < 3; j++) {
+                if (grid[i][j] == 'O') c_O++;
+                else if (grid[i][j] == 'X') c_X++;
+                else c_D++;
+            }
+        }
+        // rows
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(grid[i], "OOO") == 0) w_O++;
+            else if (strcmp(grid[i], "XXX") == 0) w_X++;
+        }
+        // collumns
+        for (int i = 0; i < 3; i++) {
+            if (grid[0][i] == 'O' && grid[1][i] == 'O' && grid[2][i] == 'O') w_O++;
+            if (grid[0][i] == 'X' && grid[1][i] == 'X' && grid[2][i] == 'X') w_X++;
+        }
+        // diagonals
+        if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid[2][2] == 'O') w_O++;
+        if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X') w_X++;
+        if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O') w_O++;
+        if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X') w_X++;
+        scanf("%c", &buf);
+
+        printf("%s\n", ((c_X == c_O || c_X == c_O + 1) && (w_X + w_O) <= 1 && !(w_O == 1 && c_X == c_O + 1) && !(w_X == 1 && c_X == c_O)) ? "yes" : "no");
+    }
+
+    return 0;
+}
