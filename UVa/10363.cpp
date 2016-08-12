6166b8008b87362dd48a6cf119b53d5378d96bf5
Elton Viana <eltonvs@outlook.com>
2016-08-12 00:08:23 -0300

    #UVa added final version from 10363

--- UVa/10363.cpp
+++ UVa/10363.cpp
@@ -3,37 +3,35 @@
 
 int main() {
     int n;
-    char grid[3][4], buf;
+    char grid[3][4];
 
-    scanf("%i", &n);
+    scanf("%i\n", &n);
 
     while (n--) {
         int c_O = 0, c_X = 0;
         int w_X = 0, w_O = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++)
             scanf("%s\n", grid[i]);
-            for (int j = 0; j < 3; j++) {
+        // count
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
                 if (grid[i][j] == 'O') c_O++;
                 else if (grid[i][j] == 'X') c_X++;
-            }
-        }
         // rows
-        for (int i = 0; i < 3; i++) {
-            if (strcmp(grid[i], "OOO") == 0) w_O++;
-            else if (strcmp(grid[i], "XXX") == 0) w_X++;
-        }
+        for (int i = 0; i < 3; i++)
+            if (!strcmp(grid[i], "OOO")) w_O++;
+            else if (!strcmp(grid[i], "XXX")) w_X++;
         // columns
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++)
             if (grid[0][i] == 'O' && grid[1][i] == 'O' && grid[2][i] == 'O') w_O++;
-            if (grid[0][i] == 'X' && grid[1][i] == 'X' && grid[2][i] == 'X') w_X++;
-        }
+            else if (grid[0][i] == 'X' && grid[1][i] == 'X' && grid[2][i] == 'X') w_X++;
         // diagonals
         if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid[2][2] == 'O') w_O++;
-        if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X') w_X++;
+        else if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X') w_X++;
         if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O') w_O++;
-        if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X') w_X++;
+        else if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X') w_X++;
 
-        printf("%s\n", ((c_X == c_O || c_X == c_O + 1) && (w_X + w_O) <= 1 && (w_O != 1 || c_X == c_O) && (w_X != 1 || c_X == c_O + 1)) ? "yes" : "no");
+        printf("%s\n", (c_X == c_O || c_X == c_O + 1) && (!w_X || !w_O) && ((!w_X && !w_O) || (w_O && c_X == c_O) || (w_X && c_X == c_O + 1)) ? "yes" : "no");
     }
 
     return 0;
